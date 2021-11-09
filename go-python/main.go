9e17ffd95ef5c9a8896a575e82bffe35e82420ee
cat <cat@catdeMac.local>
2021-11-09 10:46:25 +0800

    update go

--- go-python/main.go
+++ go-python/main.go
@@ -7,23 +7,21 @@ import (
 )
 
 func main() {
-	//var shell string = "python D:\\python\\ssh-shell\\test.py"
-	cmd := exec.Command("python", "-u","D:\\python\\ssh-shell\\ssh-shell.py", "root","192.168.10.163","22")
-	//cmd := exec.Command("python", "-u","D:\\python\\ssh-shell\\test.py")
-	reader,_ := cmd.StdoutPipe()
+	cmd := exec.Command("python3", "-u", "/Users/cat/github/go-python-ssh/sshCMD.py", "root", "192.168.10.163", "22")
+	reader, _ := cmd.StdoutPipe()
 	err := cmd.Start()
-	if err != nil{
-		fmt.Println("命令执行失败：",err)
+	if err != nil {
+		fmt.Println("命令执行失败：", err)
 		return
 	}
 	scanner := bufio.NewScanner(reader)
-	for scanner.Scan(){
+	for scanner.Scan() {
 		line := scanner.Text()
 		fmt.Println(line)
 	}
 	err = cmd.Wait()
-	if err != nil{
-		fmt.Println("cmd.Wait()命令执行失败：",err)
+	if err != nil {
+		fmt.Println("cmd.Wait()命令执行失败：", err)
 		return
 	}
 }
